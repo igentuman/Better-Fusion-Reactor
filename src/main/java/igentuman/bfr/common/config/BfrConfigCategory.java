c6d933f57a50135ea1543c7dbb920cbd9982fdfd
igentuman <igentuman@gmail.com>
2022-08-09 14:20:51 +0300

    #13 nc dependency fix

--- src/main/java/igentuman/bfr/common/config/BfrConfigCategory.java
+++ src/main/java/igentuman/bfr/common/config/BfrConfigCategory.java
@@ -1,7 +1,7 @@
 package igentuman.bfr.common.config;
 
-import nc.util.Lang;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.util.text.translation.I18n;
 import net.minecraftforge.common.config.ConfigElement;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.client.config.GuiConfig;
@@ -15,7 +15,7 @@ public interface BfrConfigCategory {
         Configuration config = BfrConfig.getConfig();
         ConfigElement newElement = new ConfigElement(config.getCategory(categoryName));
         List<IConfigElement> propertiesOnScreen = newElement.getChildElements();
-        String windowTitle = Lang.localise("gui.bfr.config.category." + categoryName);
+        String windowTitle = I18n.translateToLocalFormatted("gui.bfr.config.category." + categoryName);
         return new GuiConfig(owningScreen, propertiesOnScreen, owningScreen.modID, configElement.requiresWorldRestart() || owningScreen.allRequireWorldRestart, configElement.requiresMcRestart() || owningScreen.allRequireMcRestart, windowTitle);
     }
 }
