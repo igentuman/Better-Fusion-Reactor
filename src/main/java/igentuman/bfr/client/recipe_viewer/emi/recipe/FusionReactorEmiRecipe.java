6ee04412ce5690f511ab6e95aa6a0ca69a10b3ce
igentuman <igentuman@gmail.com>
2025-07-18 11:52:18 +0200

    Fusion EMI category

--- src/main/java/igentuman/bfr/client/recipe_viewer/emi/recipe/FusionReactorEmiRecipe.java
+++ src/main/java/igentuman/bfr/client/recipe_viewer/emi/recipe/FusionReactorEmiRecipe.java
@@ -1,8 +1,16 @@
 package igentuman.bfr.client.recipe_viewer.emi.recipe;
 
+import dev.emi.emi.api.stack.EmiStack;
 import dev.emi.emi.api.widget.WidgetHolder;
 import java.util.List;
+
+import mekanism.api.chemical.ChemicalStack;
 import mekanism.api.heat.HeatAPI;
+import mekanism.api.recipes.ingredients.ChemicalStackIngredient;
+import mekanism.api.recipes.ingredients.chemical.ChemicalIngredient;
+import mekanism.api.recipes.ingredients.chemical.EmptyChemicalIngredient;
+import mekanism.api.recipes.ingredients.chemical.SingleChemicalIngredient;
+import mekanism.api.recipes.ingredients.creator.IngredientCreatorAccess;
 import mekanism.api.text.EnumColor;
 import mekanism.client.gui.element.GuiInnerScreen;
 import mekanism.client.gui.element.gauge.GaugeType;
@@ -11,6 +19,7 @@ import mekanism.client.gui.element.gauge.GuiFluidGauge;
 import mekanism.client.recipe_viewer.emi.MekanismEmiRecipeCategory;
 import mekanism.client.recipe_viewer.emi.recipe.MekanismEmiRecipe;
 import mekanism.common.MekanismLang;
+import mekanism.common.recipe.ingredients.ChemicalIngredientCreator;
 import mekanism.common.util.MekanismUtils;
 import mekanism.common.util.UnitDisplayUtils.TemperatureUnit;
 import mekanism.common.util.text.BooleanStateDisplay.ActiveDisabled;
@@ -19,13 +28,18 @@ import igentuman.bfr.client.recipe_viewer.recipe.FusionRecipeViewerRecipe;
 import igentuman.bfr.common.BfrLang;
 import mekanism.generators.common.GeneratorsLang;
 import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.fluids.FluidStack;
 
 public class FusionReactorEmiRecipe extends MekanismEmiRecipe<FusionRecipeViewerRecipe> {
 
     public FusionReactorEmiRecipe(MekanismEmiRecipeCategory category, ResourceLocation id, FusionRecipeViewerRecipe recipe) {
         super(category, id, recipe);
-        addInputDefinition(recipe.fluidCoolant());
-        addInputDefinition(recipe.gasCoolant());
+
+        if(recipe.fluidCoolant() == null) {
+            addInputDefinition(recipe.gasCoolant());
+        } else {
+            addInputDefinition(recipe.fluidCoolant());
+        }
         addInputDefinition(recipe.fuel());
         addChemicalOutputDefinition(List.of(recipe.hotCoolant()));
     }
@@ -38,6 +52,5 @@ public class FusionReactorEmiRecipe extends MekanismEmiRecipe<FusionRecipeViewer
         initTank(widgetHolder, GuiFluidGauge.getDummy(GaugeType.STANDARD, this, 6, 13).setLabel(GeneratorsLang.FISSION_COOLANT_TANK.translateColored(EnumColor.AQUA)), input(0));
         initTank(widgetHolder, GuiChemicalGauge.getDummy(GaugeType.STANDARD, this, 25, 13).setLabel(GeneratorsLang.FISSION_FUEL_TANK.translateColored(EnumColor.DARK_GREEN)), input(1));
         initTank(widgetHolder, GuiChemicalGauge.getDummy(GaugeType.STANDARD, this, 152, 13).setLabel(GeneratorsLang.FISSION_HEATED_COOLANT_TANK.translateColored(EnumColor.GRAY)), output(0)).recipeContext(this);
-        initTank(widgetHolder, GuiChemicalGauge.getDummy(GaugeType.STANDARD, this, 6, 13).setLabel(GeneratorsLang.FISSION_COOLANT_TANK.translateColored(EnumColor.AQUA)), input(0)).recipeContext(this);
     }
 }
